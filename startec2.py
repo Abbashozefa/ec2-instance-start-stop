0ab78efbb7e41faf1348a8da6e311e003897ff3a
Abbas <abbashozefa@gmail.com>
2023-07-13 16:16:07 +0530

    commit:

--- startec2.py
+++ startec2.py
@@ -2,7 +2,6 @@ import boto3
 region='us-east-1'
 instances=['']
 ec2=boto3.client('ec2',region_name=region)
-def lambda_handler(event, context):
-    # TODO implement
-    ec2.start_instances(InstanceIds=instances)
-    print("started your instances"+str(instances))
+# TODO implement
+ec2.start_instances(InstanceIds=instances)
+print("started your instances"+str(instances))
